fd22740d61c9d96b0222134e41c380cc2c7d217c
solael <yuheng.zhao067@gmail.com>
2017-01-11 05:09:42 +0100

    init

---
+++ app/src/main/java/com/ensiie/yuheng/moviie/MovieDetailActivity.java
@@ -0,0 +1,30 @@
+package com.ensiie.yuheng.moviie;
+
+import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MovieDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle(getString(R.string.movie_name));
+
+        final ImageView imageView = (ImageView) findViewById(R.id.image);
+        Picasso.with(this).load("http://livehdwallpaper.com/wp-content/uploads/2016/10/Sunset-Photos.jpg").into(imageView);
+    }
+
+
+}
